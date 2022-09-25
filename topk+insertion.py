d06fb9a6dd63a79c2c20b6e2d3dc2f2d069760dd
matheusvrs <matheusparaiso98@gmail.com>
2022-09-25 08:17:34 -0300

    add topk frequent elements + insertion sort

--- topk+insertion.py
+++ topk+insertion.py
@@ -1,29 +1,42 @@
 import time
 
-def insertion_sort(elementos):
-    pass
+def insertion_sort(elements):
+	for i in range(1, len(elements)):
+		key = elements[i]
+		j = i-1
 
+		while j >=0 and key[1] > elements[j][1] :
+				elements[j+1] = elements[j]
+				j -= 1
+		elements[j+1] = key
 
-def topk_fe(elementos, k):
+	return elements
+
+def topK(elements, K):
+	N = len(elements)
+	freq_mp = {}
+	for i in range(N):
+		if elements[i] in freq_mp:
+			freq_mp[elements[i]] += 1
+		else:
+			freq_mp[elements[i]] = 1
+
+	a = [0] * (len(freq_mp))
+	j = 0
+	for i in freq_mp:
+		a[j] = [i, freq_mp[i]]
+		j += 1
+
+	a = insertion_sort(a)
+
+	for i in range(K):
+		print(a[i][0])
+
+if __name__ == "__main__":
+    elements = [3, 1, 1, 1, 4, 4, 4, 5, 2, 6, 1]
+    K = 2
     tic = time.time()
-    #insertion_sort(elementos)
-    if k <= 0:
-        return None
-
-    k_elem = [] # primeiro sempre é o mais frequente.
-    ant = elementos[0]
-    i = 1
-    while len(k_elem) < k:
-        if elementos[i] != ant:
-            k_elem.append(elementos[i])
-        ant = elementos[i]
-        i += 1
-
+    topK(elements, K)
     toc = time.time()
-    print(k_elem)
-    print(f'Tempo de processamento: {abs(tic - toc)} s')
+    print(f'runtime: {toc-tic} seconds')
 
-if __name__ == '__main__':
-    elementos = [1, 3, 5, 5, 6, 7, 7, 8, 8, 8]
-    k = 2
-    topk_fe(elementos, k)
